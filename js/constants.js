ac9b5337e1a4a7f526b8a54c52304438adfd2e91
and0x01 <107023374+and0x01@users.noreply.github.com>
2022-06-21 20:14:41 -0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,3 @@
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-const API_ACCESS_TOKEN = '';
+const API_ACCESS_TOKEN = '74050a6a1b13433fcc1cf25c57fce2ff';
 const API_IMAGE_URL = 'http://image.tmdb.org/t/p/';
